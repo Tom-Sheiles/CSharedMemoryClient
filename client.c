ff89f69130f4df6001a3cdbf87a2fa37db4c905e
root <root@Wookie-PC.localdomain>
2019-09-22 20:40:18 +1000

    Added dynamic arrays to client to hold server return and separate the output of each thread

--- client.c
+++ client.c
@@ -18,26 +18,53 @@ typedef struct{
 	int inputNumber;
 }returnInfo;
 
+typedef struct{
+	unsigned int *array;
+	size_t used;
+	size_t size;
+}Vector;
+
+void CreateVector(Vector *a, size_t initSize){
+	a->array = (unsigned int *)malloc(initSize * sizeof(unsigned int));
+	a->used = 0;
+	a->size = initSize;
+}
+
+void pushVector(Vector *a, unsigned int x){
+	if(a->used == a->size){
+		a->size *= 2;
+		a->array = (unsigned int *)realloc(a->array, a->size * sizeof(unsigned int));
+	}
+	a->array[a->used++] = x;
+}
+Vector factors[10];
+
 void *serverOutput(){
 	while(1){
 		for(int i = 0; i < 10; i++){
-			//sleep(2);
+			
 			if(shmptr->serverFlag[i] == 1){
-				printf("%u ", shmptr->slot[i]);
-				fflush(stdout);
+				//printf("%u ", shmptr->slot[i]);
+				//fflush(stdout);
+				pushVector(&factors[i], shmptr->slot[i]);
 				shmptr->serverFlag[i] = 0;
 			}	
 		}
+		
 	}
 }
+
 
 void handleInput(){
 
 	pthread_t resultThread;
 	pthread_create(&(resultThread), NULL, &serverOutput, NULL);
+	
 
 	char buffer[1];
 	while(shmptr->number != -2){
+
+	
 
 		if(shmptr->clientFlag == 0){
 			fgets(buffer, 4, stdin);
@@ -45,8 +72,23 @@ void handleInput(){
 			int number = atoi(buffer);
 			shmptr->number = number;
 			shmptr->clientFlag = 1;
+
+			// ########TODO FINISH PRINT STATEMENTS FROM HERE AND DO PROGRESS BAR
+			if(atoi(buffer) == -3){
+				
+				for(int j = 0; j < 10; j++){
+					printf("Slot %d\n",j);
+					for(int k = 0; k < factors[j].used; k++){
+						printf("%u ",factors[j].array[k]);
+					}
+					printf("\n");
+				}
+				shmptr->number = -2;
+				return;
+			}
 			if(atoi(buffer) == -2)
 				return;
+
 			bzero(buffer, sizeof(buffer));
 
 			//Wait for server to accept request
@@ -54,8 +96,8 @@ void handleInput(){
 			if(shmptr->number != -1){
 				printf("Server Started request in slot: %d\n",shmptr->number);
 				inputNumbers[shmptr->number] = number;
-				//returnInfo *returninfo = malloc(sizeof(returninfo));
-				//returninfo->inputNumber = number; // ############### TODO HERE PRINT FOR ALL THREADS
+				CreateVector(&factors[shmptr->number], 100);
+				
 			}else{
 				printf("Server cannot handle more than 10 requests\n");
 			}
